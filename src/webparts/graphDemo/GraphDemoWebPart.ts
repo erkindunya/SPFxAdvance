411dd92ebb14009f2dd55ec64b56ccc3114911de
Enver Selchuk <Enver.Selchuk@gamma.co.uk>
2020-08-28 14:09:34 +0100

    With MSGraphClient code

--- src/webparts/graphDemo/GraphDemoWebPart.ts
+++ src/webparts/graphDemo/GraphDemoWebPart.ts
@@ -9,7 +9,7 @@ import { escape } from '@microsoft/sp-lodash-subset';
 import styles from './GraphDemoWebPart.module.scss';
 import * as strings from 'GraphDemoWebPartStrings';
 
-import { AadHttpClient } from "@microsoft/sp-http";
+import { AadHttpClient, MSGraphClient } from "@microsoft/sp-http";
 
 export interface IGraphDemoWebPartProps {
   description: string;
@@ -25,15 +25,22 @@ export default class GraphDemoWebPart extends BaseClientSideWebPart<IGraphDemoWe
             <div class="${ styles.column }">
               <span class="${ styles.title }">Graph API Output!</span>
               <p class="${ styles.subTitle }">Information returned via Graph API.</p>
+              <h3>Current User</h3>
               <p class="${ styles.description }" id="userinfo">
                 Loading...
               </p>
+              <h3>List of Users</h3>
+              <p class="${ styles.description }" id="users">
+                Loading...
+              </p>  
             </div>
           </div>
         </div>
       </div>`;
 
-      this.getUserInfo();
+      this.getUserInfo(); // Uses AADHttpClient
+
+      this.getAllUsers(); // Uses MS Graph Client
   }
 
   protected get dataVersion(): Version {
@@ -72,7 +79,7 @@ export default class GraphDemoWebPart extends BaseClientSideWebPart<IGraphDemoWe
           .then(data => {
               let html : string = `<div>
                 Name: ${ data.displayName } <br/>
-                Email: ${ data.email }
+                Email: ${ data.mail }
               </div>`;
 
             this.domElement.querySelector("#userinfo").innerHTML = html;
@@ -80,6 +87,33 @@ export default class GraphDemoWebPart extends BaseClientSideWebPart<IGraphDemoWe
           .catch(err=> {
             this.domElement.querySelector("#userinfo").innerHTML = `<div>Error getting user info: ${err}</div>`;
           });
+      })
+  }
+
+  private getAllUsers() {
+    this.context.msGraphClientFactory.getClient()
+      .then((client: MSGraphClient) => {
+          client.api("users")
+            .version("v1.0")
+            .select("displayName,mail,userPrincipalName")
+            .get((err,res) => {
+              if(err) {
+                console.log("Error fetching users :"  +err);
+                return;
+              }
+
+              let html = "";
+
+              for(let u of res.value) {
+                html += `<div>
+                  ${ u.displayName } <br/>
+                  ${ u.mail } <br/>
+                  ${ u.userPrincipalName }
+                </div>`;
+
+                this.domElement.querySelector("#users").innerHTML = html;
+              }
+            });
       })
   }
 }
